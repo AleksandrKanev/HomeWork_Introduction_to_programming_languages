f2486ed5c7f959329f67a1109b8821736c00024a
Kanev Aleksandr <kanev085@yandex.ru>
2022-11-28 20:00:53 +0300

    ДЗ 9

---
+++ HomeWork_009/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void ShowNums(int n)
+{
+    Console.Write(n + ", ");
+    if (n > 1) ShowNums(n - 1);
+
+}
+
+//ShowNums(5);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumOfDigit(int m, int n)
+{
+    if (m < n - 1) return SumOfDigit(m + 1, n) + m + 1;
+    return 0;
+}
+
+// Console.WriteLine(SumOfDigit(1, 6));
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akk(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akk(n - 1, 1);
+    if (n > 0 && m > 0) return Akk(n - 1, Akk(n, m - 1));
+    return Akk(n, m);
+}
+
+Console.WriteLine(Akk(3, 3));
